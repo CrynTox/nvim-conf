8d433f9c032e922e20a5c01dbe7b204d83b55121
CrynTox <88785107+CrynTox@users.noreply.github.com>
2025-07-31 22:46:50 +0300

    feat: setup options

---
+++ lua/core/options.lua
@@ -0,0 +1,44 @@
+local opts = {
+  breakindent = true,
+  completeopt = {
+    "fuzzy",
+    "menuone",
+    "noinsert",
+    "popup",
+  },
+  confirm = true,
+  cursorline = true,
+  expandtab = true,
+  foldlevel = 99,
+  hlsearch = false,
+  ignorecase = true,
+  inccommand = "split",
+  laststatus = 3,
+  list = true,
+  listchars = {
+    tab = "» ",
+    trail = "·",
+    nbsp = "⍽",
+  },
+  number = true,
+  relativenumber = true,
+  scrolloff = 10,
+  shiftwidth = 4,
+  sidescrolloff = 10,
+  signcolumn = "yes",
+  smartcase = true,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  tabstop = 4,
+  timeoutlen = 250,
+  undofile = true,
+  undolevels = 10000,
+  updatetime = 250,
+  virtualedit = { "block" },
+  wrap = false,
+}
+
+for k, v in pairs(opts) do
+  vim.opt[k] = v
+end
