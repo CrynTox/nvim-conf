bdf9997e210e66c58a51e64d147c6fb183270e72
CrynTox <88785107+CrynTox@users.noreply.github.com>
2025-08-01 16:45:07 +0300

    feat: setup snacks picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,5 +1,27 @@
 return {
   "folke/snacks.nvim",
+  lazy = false,
   priority = 1000,
-  opts = {},
+  dependencies = {
+    "which-key.nvim",
+  },
+  opts = {
+    picker = { enabled = true },
+  },
+  config = function(_, opts)
+    require("snacks").setup(opts)
+
+    local wk = require("which-key")
+    wk.add({
+      { "<Leader>s", group = "[S]earch" },
+    })
+  end,
+  keys = {
+    { "<Leader>sf", function() Snacks.picker.files() end, desc = "[S]earch [F]iles" },
+    { "<Leader>sF", function() Snacks.picker.git_files() end, desc = "[S]earch Git [F]iles" },
+    { "<Leader>sg", function() Snacks.picker.grep() end, desc = "[S]earch [G]rep" },
+    { "<Leader>sh", function() Snacks.picker.help() end, desc = "[S]earch [H]elp" },
+    { "<Leader>su", function() Snacks.picker.undo() end, desc = "[S]earch [U]ndo" },
+    { "<Leader>sj", function() Snacks.picker.jumps() end, desc = "[S]earch [J]umps" },
+  },
 }
