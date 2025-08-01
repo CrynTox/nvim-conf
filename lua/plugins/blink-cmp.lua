ffc377f469cb8f79eca0e89d737b8dadd47dc5ca
CrynTox <88785107+CrynTox@users.noreply.github.com>
2025-08-01 20:03:50 +0300

    feat: setup blink.cmp to use mini.icons

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -2,20 +2,45 @@ return {
   "saghen/blink.cmp",
   dependencies = { "rafamadriz/friendly-snippets" },
   version = "1.*",
-  opts = {
-    keymap = { preset = "default" },
-    completion = {
-      list = {
-        selection = {
-          preselect = false,
-          auto_insert = true,
+  opts = function()
+    local mini_icons = require("mini.icons")
+    return {
+      keymap = { preset = "default" },
+      completion = {
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = true,
+          },
         },
+        menu = {
+          draw = {
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  local kind_icon, _, _ = mini_icons.get("lsp", ctx.kind)
+                  return kind_icon
+                end,
+                highlight = function(ctx)
+                  local _, hl, _ = mini_icons.get("lsp", ctx.kind)
+                  return hl
+                end,
+              },
+              kind = {
+                highlight = function(ctx)
+                  local _, hl, _ = mini_icons.get("lsp", ctx.kind)
+                  return hl
+                end,
+              },
+            },
+          },
+        },
+        documentation = { auto_show = true },
       },
-      documentation = { auto_show = true },
-    },
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-    },
-    signature = { enabled = true },
-  },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+      signature = { enabled = true },
+    }
+  end,
 }
