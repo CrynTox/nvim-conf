4a5c201401dcf269d88a6bebb5fdafb761ec9923
CrynTox <88785107+CrynTox@users.noreply.github.com>
2025-08-01 16:14:48 +0300

    feat: add noice.nvim

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/noice.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  opts = {
+    cmdline = {
+      opts = {
+        position = {
+          row = "25%",
+          col = "50%",
+        },
+      },
+    },
+  },
+}
