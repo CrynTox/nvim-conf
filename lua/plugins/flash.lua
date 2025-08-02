f0454846dc0364b1dee736a45d8b9376b72621cc
CrynTox <88785107+CrynTox@users.noreply.github.com>
2025-08-02 23:52:38 +0300

    feat: add flash.nvim

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/flash.nvim",
+  opts = {
+    modes = {
+      char = {
+        keys = { "f", "F", "t", "T" },
+        char_actions = function()
+          return {
+            [";"] = "next",
+            [","] = "prev",
+          }
+        end,
+      },
+    }
+  },
+  keys = {
+    { "s", function() require("flash").jump() end, desc = "[S]eek Flash" },
+    { "<Leader>S", function() require("flash").treesitter() end, desc = "[S]eek Treesitter" },
+    { "f" }, { "F" }, { "t" }, { "T" }
+  },
+}
