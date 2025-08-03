82fc7bb418784dac348be4938bb5f3395ae06eb0
CrynTox <88785107+CrynTox@users.noreply.github.com>
2025-08-03 13:57:04 +0300

    feat: add conform.nvim and setup stylua

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+    },
+    format_on_save = {
+      lsp_format = "fallbak",
+      timeout_ms = 500,
+    },
+  },
+  config = function(_, opts)
+    require("conform").setup(opts)
+    vim.opt.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+}
