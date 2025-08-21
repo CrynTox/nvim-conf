06e2e99b863ffea8dfd50f30ebf59d8a9f4a69a1
CrynTox <88785107+CrynTox@users.noreply.github.com>
2025-08-21 20:02:15 +0300

    fix: remove useless spaces

--- lua/core/misc.lua
+++ lua/core/misc.lua
@@ -5,10 +5,10 @@ vim.diagnostic.config({
   float = true,
   signs = {
     text = {
-      [vim.diagnostic.severity.ERROR] = " ",
-      [vim.diagnostic.severity.WARN] = " ",
-      [vim.diagnostic.severity.HINT] = " ",
-      [vim.diagnostic.severity.INFO] = " ",
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.HINT] = "",
+      [vim.diagnostic.severity.INFO] = "",
     },
   },
 })
