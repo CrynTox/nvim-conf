ea45906ed398d3da3ac12333aad4b31e98ca5198
CrynTox <88785107+CrynTox@users.noreply.github.com>
2025-09-03 21:54:51 +0300

    feat: setup java-debug-adapter with jdtls

--- lua/plugins/nvim-jdtls.lua
+++ lua/plugins/nvim-jdtls.lua
@@ -1,11 +1,25 @@
 return {
   "mfussenegger/nvim-jdtls",
   dependencies = {
-    "mason-lspconfig.nvim",
-    opts = function(_, opts)
-      opts = opts or {}
-      opts.ensure_installed = opts.ensure_installed or {}
-      table.insert(opts.ensure_installed, "jdtls")
-    end,
+    {
+      "mason-lspconfig.nvim",
+      opts = function(_, opts)
+        opts = opts or {}
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, "jdtls")
+      end,
+    },
+    "nvim-dap",
   },
+  config = function()
+    vim.lsp.config("jdtls", {
+      init_options = {
+        bundles = {
+          vim.fn.stdpath("data") .. "/mason/share/java-debug-adapter/com.microsoft.java.debug.plugin.jar",
+        },
+      },
+    })
+
+    require("jdtls").setup_dap()
+  end,
 }
