d0640db2ec73d1ebc219924d47f150f80aa3c254
CrynTox <88785107+CrynTox@users.noreply.github.com>
2025-10-09 16:30:56 +0300

    feat: add treesitter-modules.nvim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,11 @@
 return {
-  -- TODO: implement automatic treesitter enabling
-  "nvim-treesitter/nvim-treesitter",
-  branch = "main",
-  build = ":TSUpdate",
+  "MeanderingProgrammer/treesitter-modules.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  ---@module 'treesitter-modules'
+  ---@type ts.mod.UserConfig
+  opts = {
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
 }
